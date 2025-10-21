32dec259134c36c1f1eafe3779b14f999e73f524
Josue Castro <josuecstro2023@gmail.com>
2025-10-20 19:20:43 -0600

    Tarea de Guias finalizada

---
+++ proyecto_fineconia/app/Models/Favorito.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Favorito extends Model
+{
+    protected $table = 'favoritos';
+    protected $fillable = ['user_id', 'guia_path'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
